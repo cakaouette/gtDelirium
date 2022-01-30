a5633e2d9e7c17caa3c502f85da58e1f0be1612c
Sshnyari <sshnyari@users.noreply.github.com>
2022-01-30 13:46:07 +0100

    fixing auth for latest routes

--- config/routes.php
+++ config/routes.php
@@ -28,13 +28,13 @@ return function (App $app) {
         })->add(Auth::class);
         $group->group('/tip', function ($tip) {
             $tip->get('', fn ($request, $response) => $this->get(Twig::class)->render($response, 'tip/index.twig'))->setName('tip')->setArgument('content-title', 'Liens & Astuces');
-        })->add(Auth::class);
+        });
         $group->group('/conquest', function ($tip) {
             $tip->get('/priority', fn ($request, $response) => $this->get(Twig::class)->render($response, 'conquest/priority.twig'))->setName('conquest-priority')->setArgument('content-title', 'Conquête en cours');
             $tip->get('/old', fn ($request, $response) => $this->get(Twig::class)->render($response, 'conquest/old.twig'))->setName('conquest-old')->setArgument('content-title', 'Stratégie de conquête');
             $tip->get('/tremens', fn ($request, $response) => $this->get(Twig::class)->render($response, 'conquest/tremens.twig'))->setName('conquest-tremens')->setArgument('content-title', 'Stratégie des Tremens');
             $tip->get('/nocturnum', fn ($request, $response) => $this->get(Twig::class)->render($response, 'conquest/nocturnum.twig'))->setName('conquest-nocturnum')->setArgument('content-title', 'Stratégie des Nocturnums');
-        })->add(Auth::class);
+        });
         $group->group('/alliance', function ($home) {
             $home->get('', [C\AllianceController::class, 'index'])->setName('alliance')->setArgument('content-title', 'L\'alliance Délirium');
             $home->map(['GET', 'POST'], '/guild', [C\AllianceController::class, 'new'])->setName('alliance-new-guild')->setArgument('content-title', 'Nouvelle guilde');
